686fc6b04fc9e71f44d079ddf306c88c094c757c
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-06 18:37:38 +0530

    improve the code readability

--- restapi/schema.py
+++ restapi/schema.py
@@ -1,4 +1,5 @@
 import graphene
+from graphene import ObjectType
 from graphene_django import DjangoObjectType
 from .models import CategoryModel, DrinkModel
 
@@ -12,23 +13,23 @@ class CategoryType(DjangoObjectType):
         model = CategoryModel
         fields = '__all__'
 
-class Query(graphene.ObjectType):
-    drinks = graphene.List(DrinkType)
-    drink = graphene.Field(DrinkType,id=graphene.String())
-    categories = graphene.List(CategoryType)
-    category = graphene.Field(CategoryType,id=graphene.String())
+class Query(ObjectType):
     
+    drinks = graphene.List(DrinkType)
     def resolve_drinks(root,info):
         return DrinkModel.objects.all()
     
+    drink = graphene.Field(DrinkType,id=graphene.String())
     def resolve_drink(root,info,id):
         return DrinkModel.objects.get(id=id)
     
+    categories = graphene.List(CategoryType)
     def resolve_categories(root,info):
         return CategoryModel.objects.all()
     
+    category = graphene.Field(CategoryType,id=graphene.String())
     def resolve_category(root,info,id):
         return CategoryModel.objects.get(id=id)
     
-    
+ 
 schema = graphene.Schema(query=Query)
