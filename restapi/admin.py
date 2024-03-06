83ed53293090b62c77b3651fcd2459291ff3d17c
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-06 13:29:28 +0530

    register category model to the admin

--- restapi/admin.py
+++ restapi/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from restapi.models import DrinkModel
+from restapi.models import CategoryModel, DrinkModel
 
 # Register your models here.
 admin.site.register(DrinkModel)
+admin.site.register(CategoryModel)
