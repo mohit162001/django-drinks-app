f23fad9944660a5253aba9663d04a800f53ad828
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-08 16:05:10 +0530

    remove unneccessary classes

--- drinks/urls.py
+++ drinks/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
@@ -27,7 +24,5 @@ from graphene_django.views import GraphQLView
 urlpatterns = [
     path('admin/', admin.site.urls),
      path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('',include('restapi.urls'))
 ]
