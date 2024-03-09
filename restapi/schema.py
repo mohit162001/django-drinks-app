ce6b968cfb96bc8f7fe6324a9703cc5446d0c7d0
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-09 11:37:00 +0530

    add mutation for create user and get token also add query authentication

--- restapi/schema.py
+++ restapi/schema.py
@@ -1,6 +1,11 @@
 import graphene
 from graphene_django import DjangoObjectType
 from .models import CategoryModel, DrinkModel
+from django.contrib.auth import get_user_model
+from graphene_django import DjangoObjectType
+import graphql_jwt
+from graphql_jwt.decorators import login_required
+
 
 class DrinkType(DjangoObjectType):
     class Meta:
@@ -12,9 +17,17 @@ class CategoryType(DjangoObjectType):
         model = CategoryModel
         fields = '__all__'
 
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+        
+        
 class Query(graphene.ObjectType):
-    drinks = graphene.List(DrinkType)
+    drinks = graphene.List(DrinkType,)
     def resolve_drinks(root,info):
+        user = info.context.user
+        if not user.is_authenticated:
+            raise Exception("Authentication credentials were not provided")
         return DrinkModel.objects.all()
     
     # filter_drinks_by_desc = graphene.List(DrinkType,search = graphene.String())
@@ -41,10 +54,12 @@ class Query(graphene.ObjectType):
 
     def resolve_filter_drink(root, info, name=None, desc=None, price=None):
         drinks = DrinkModel.objects.all()
-
-        if name:
+        user = info.context.user
+        if not user.is_authenticated:
+            raise Exception("Authentication credentials were not provided")
+        if name is not None:
             drinks = drinks.filter(name__istartswith=name)
-        if desc:
+        if desc is not None:
             drinks = drinks.filter(desc__icontains=desc)
         if price is not None:
             drinks = drinks.filter(price=price)
@@ -53,11 +68,17 @@ class Query(graphene.ObjectType):
 
     drink = graphene.Field(DrinkType,id=graphene.String())
     def resolve_drink(root,info,id):
+        user = info.context.user
+        if not user.is_authenticated:
+            raise Exception("Authentication credentials were not provided")
         return DrinkModel.objects.get(id=id)
     
     
     categories = graphene.List(CategoryType)
     def resolve_categories(root,info):
+        user = info.context.user
+        if not user.is_authenticated:
+            raise Exception("Authentication credentials were not provided")
         print(info.operation)
         print(root)
         return CategoryModel.objects.all()
@@ -65,7 +86,22 @@ class Query(graphene.ObjectType):
     
     category = graphene.Field(CategoryType,id=graphene.String())
     def resolve_category(root,info,id):
+        user = info.context.user
+        if not user.is_authenticated:
+            raise Exception("Authentication credentials were not provided")
         return CategoryModel.objects.get(id=id)
+    
+    users = graphene.List(UserType)
+
+    def resolve_users(self, info):
+        return get_user_model().objects.all()
+    
+    me = graphene.Field(UserType)
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not logged in!')
+        return user
       
 
 class DrinkCreate(graphene.Mutation):
@@ -95,6 +131,7 @@ class DrinkUpdate(graphene.Mutation):
     drink = graphene.Field(DrinkType)
 
     @classmethod
+    @login_required
     def mutate(cls,root,info,id,name=None,desc=None,price=None):
         if name is not None:
             DrinkModel.objects.filter(id=id).update(name=name)
@@ -119,12 +156,34 @@ class DrinkDelete(graphene.Mutation):
         drink = DrinkModel.objects.get(id=id)
         drink.delete()
         return DrinkDelete(message = "Drink delelted successfully")
-    
-    
+
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    def mutate(self, info, username, password, email):
+        user = get_user_model()(
+            username=username,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+
+        return CreateUser(user=user)
+      
 class Mutation(graphene.ObjectType):
     update_drink = DrinkUpdate.Field()
     create_drink = DrinkCreate.Field()
     delete_drink = DrinkDelete.Field()
-    
+    create_user = CreateUser.Field()
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
         
 schema = graphene.Schema(query=Query,mutation=Mutation)
